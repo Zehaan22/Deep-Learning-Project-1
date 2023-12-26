adf63758d22556ac04f15ea304175e2efe64a79c
Zehaan <zehaan22@iitk.ac.in>
2023-12-26 14:47:58 +0530

    Added the data Storage stuff.

--- GUI/main.py
+++ GUI/main.py
@@ -2,6 +2,8 @@
 
 import pygame
 import pygame_gui as pg
+import torch
+import numpy as np
 pygame.init()
 
 # Defining Window variables
@@ -83,6 +85,36 @@ class DrawingSurface:
             for j in range(self.DIMS[1]):
                 self.grid[i][j].color = 0
 
+    def store_board(self, value):
+        """Store the drawing surface."""
+
+        # Creating the array to store the value
+        array = []
+        for i in range(self.DIMS[0]):
+            for j in range(self.DIMS[1]):
+                array.append(self.grid[i][j].color)
+
+        # Loading the data tensore
+        X_data = torch.load("X_data.pt")
+        X_data = X_data.numpy()
+        # Adding our element to the data
+        X_data = np.append(X_data, [array], axis=0)
+        X_data = torch.from_numpy(X_data)
+        # Saving the data matrix
+        torch.save(X_data, "X_data.pt")
+        print("Saved the matrix")
+
+        # Saving the label
+        y_data = torch.load("y_data.pt")
+        y_data = y_data.numpy()
+        # Adding our element to the data
+        label = value
+        y_data = np.append(y_data, [label])
+        y_data = torch.from_numpy(y_data)
+        # Saving the data matrix
+        torch.save(y_data, "y_data.pt")
+        print(f"Saved as the lable {label}")
+
 
 class MainRun:
     """Class to contain the main functions to run and manage the GUI window."""
@@ -92,7 +124,7 @@ class MainRun:
         # Setting the loop variables
         self.run = True
         self.CLOCK = pygame.time.Clock()
-        self.FPS = 60
+        self.FPS = 80
 
         # Creating the tiles
         self.sketch_board = DrawingSurface()
@@ -101,11 +133,17 @@ class MainRun:
         self.manager = pg.UIManager((WIDTH, HEIGHT))
 
         # Creating the GUI Elements
-        foo_rect = pygame.Rect((900, 650), (200, 60))
+        foo_rect = pygame.Rect((750, 650), (200, 60))
         self.clear_btn = pg.elements.UIButton(
             relative_rect=foo_rect,
             text='Clear Board',
             manager=self.manager)  # Button to clear the board
+
+        foo_rect = pygame.Rect((950, 650), (200, 60))
+        self.store_btn = pg.elements.UIButton(
+            relative_rect=foo_rect,
+            text='Store Matrix',
+            manager=self.manager)  # Button to store the board
 
         foo_rect = pygame.Rect((50, 50), (800, 50))
         self.title_bar = pg.elements.UITextBox(
@@ -119,7 +157,14 @@ class MainRun:
             "My Best Guess is : <br> <b>9</b>",
             foo_rect,
             self.manager
-        )
+        )  # Prediction bar
+
+        foo_rect = pygame.Rect((750, 250), (100, 30))
+        self.input_box = pg.elements.UITextEntryBox(
+            foo_rect,
+            "1",
+            self.manager
+        )  # Input Box to take the number
 
     def update_display(self):
         """Update varoius elements of the display."""
@@ -144,8 +189,14 @@ class MainRun:
     def handel_UI_events(self, event):
         """Handel UI events thorugh PyGame_GUI."""
         if event.user_type == pg.UI_BUTTON_PRESSED:
+            # Clear the board
             if event.ui_element == self.clear_btn:
                 self.sketch_board.clear_board()
+
+            # Store the board
+            if event.ui_element == self.store_btn:
+                value = self.input_box.get_text()  # Getting the input from the user
+                self.sketch_board.store_board(int(value))
 
     def run_window(self):
         """Run an iteration of the window."""
---
+++ Learning Model/test.py
@@ -0,0 +1,7 @@
+"""File to test out approaches to the code."""
+
+import torch
+import numpy
+
+foo_tens = torch.load("y_data.pt")
+print(foo_tens)
